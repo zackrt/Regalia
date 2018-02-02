67050576c48c40a402078f9b1b506cc02a4d2bbe
Zack Toth <zacharyrtoth@gmail.com>
2018-02-01 18:59:25 -0800

    added to blockchain.js

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -3,3 +3,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET blockchain page. */
+
+//first attempt
+
+const SHA256 = require("crypto-js/sha256");
+
+//Create the class Block with parameters = index, timestamp, data & previous hash
+class Block {
+        constructor(index, timestamp, data, previous = '') {
+            this.index = index;
+            this.previousHash;
+            this.timestamp = timestamp;
+            this.data = data;
+            this.hash = this.calculateHash();
+            this.nonce = 0;
+
+        }
+
+        calculateHash() {
+            return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+        }
+}
