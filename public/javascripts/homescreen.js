602970b9a7d7558f4e8eda30e95db7615dc1ac4c
Zack Toth <zacharyrtoth@gmail.com>
2018-02-03 10:55:02 -0800

    working with ted

--- public/javascripts/homescreen.js
+++ public/javascripts/homescreen.js
@@ -1,5 +1,13 @@
 'use strict';
 
 $(function(){
-    
+    $('body').on('click', '.TEST-BLOCKCHAIN', function(){
+        alert("this is working");
+        $.ajax(
+            {url: '/get_blockchain', 
+            type: 'POST', 
+            data: { index: index, user: user }, 
+            success: function(result) 
+            { alert('success from POST') } });
+    }
 })
--- routes/blockchain.js
+++ routes/blockchain.js
@@ -1,87 +1,87 @@
-'use strict';
-
-
 var express = require('express');
 var router = express.Router();
-import { BADFAMILY } from 'dns';
-/* GET blockchain page. */
-// this needs endpoint handles
-//first attempt
+const { User } = require('../models/users');
+/* GET users listing. */
 router.post('/', function(req, res, next) {
-    res.status(200).json({ messsage: "I am here" }); 
+    res.status(200).json({ messsage: "I am here with 1325 regalia" }); 
+  // console.log(req.body)
+  // console.log(req.body.id)
+//   try {
+    // let newBlockchain = new Blockchain();
+//     User.findOneAndUpdate({ _id: req.body.id}, 
+//         { $push: { blockchains: newBlockchain } }, 
+//         {new: true}, 
+//         function (err, user) {
+//             if(err) {
+//                 res.status(500).json({ message: 'Internal server error' });
+//             } else {
+//                 res.status(200).json({ user: user });
+//             }
+//     });
+//   } catch (e) {
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
 });
-
+router.delete('/d', function(req, res, next) {
+  // console.log(req.body)
+   console.log('delete = ' + req.body.index)
+   console.log('delete = ' + req.body.user)
+  try {
+    let newBlockchain = new Blockchain();
+    User.delete({ name: req.body.user}, 
+      { $push: { blockchains: newBlockchain } }, 
+      {new: true}, 
+      function (err, user) {
+        if(err) {
+          res.status(500).json({ message: 'Internal server error' });
+        } else {
+          res.status(200).json({ user: user });
+        }
+  });
+  } catch (e) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 const SHA256 = require("crypto-js/sha256");
-
-//Create the class Block with parameters = index, timestamp, data & previous hash
-// class Block {
-//         constructor(index, timestamp, data, previous = '') {
-//             this.index = index;
-//             this.previousHash;
-//             this.timestamp = timestamp;
-//             this.data = data;
-//             this.hash = this.calculateHash();
-//             this.nonce = 0;
-
-//         }
-
-//         calculateHash() {
-//             return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-//         }
-
-//         // mineBlock(difficulty) {
-//         //    while (this.hash.substring(0. difficulty !== Array(difficulty + 1).join("0")) {
-//         //        this.nonce++;
-//         //         this.hash = this.calculateHash();
-//         //     }
-
-//         //     console.log("BLOCK MINED: " + this.hash);
-//         // }
-// }
-
-
-// class Blockchain{
-//     constructor() {
-//         this.chain = [this.createGenesisBlcok()];
-//         this.difficulty = 5;
-//     }
-
-//     createGenesisBlock() {
-//         return new Block(0, "02/03/2017", "Genesis block", "0");
-//     }
-
-//     getLatestBlock() {
-//         return this.chain[this.chain.length - 1];
-//     }
-
-//     addBlock(newBlock) {
-//         newBlock.previousHash = this.getLatestBlock().hash;
-//         newBlock.mineBlock(this.difficulty);
-//         this.chain.push(newBlock);
-//     }
-//     // to validate blockchain
-//     isChainValid() {
-//         for (let i = 1; i < this.chain.length; i++) {
-//             const currentBlock = this.chain[i];
-//             const previousBlock = this.chain[i - 1];
-
-//             if (currentBlock.hash !== currentBlock.calculateHash()) {
-//                 return false;
-//             }
-
-//             if (currentBlock.previousHash !== previousBlock.hash) {
-//                 return false;
-//             }
-
-//         }
-
-//         return true;
-//     }
-// }
-// //to create regalia connected to rent payments in feb 2, 2018
-// let regalia = new Blockchain();
-// console.log('Mining block 1...');
-// regalia.addBlock(new Block(1 ,"02/02/018", { amount: 1325}));
-
-// console.log('Mining block 2...');
-// regalia.addBlock(new Block(2, "02/02/2018", { amount: 1950}));
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.calculateHash();
+    }
+    calculateHash() {
+      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+class Blockchain{
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+    createGenesisBlock() {
+        return new Block(0, "01/01/2017", "Genesis block", "0");
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+module.exports = router;
