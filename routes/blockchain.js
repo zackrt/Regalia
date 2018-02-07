628775724c4fc450260739e67a286d5e02abafdb
ted <ted@warpedpuppy.com>
2018-02-06 19:21:06 -0800

    cleaned code

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -3,16 +3,33 @@ var router = express.Router();
 const { User } = require('../models/users');
 
 
-router.post('/', function(req, res) {
+router.get('/', function(req, res) {
     try {
-        res.status(200).json({ message: "I am here with 1325 regalia" }); 
+        res.status(200).json({ message: "blockchain.js get was hit!" }); 
     } catch (e) {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
 
 
-router.delete('/delete', function(req, res) {
+router.put('/', function(req, res) {
+    try {
+        res.status(200).json({ message: "blockchain.js put was hit!" }); 
+    } catch (e) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+router.post('/', function(req, res) {
+    try {
+        res.status(200).json({ message: "blockchain.js post was hit!" }); 
+    } catch (e) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+
+router.delete('/', function(req, res) {
     try {
         res.status(200).json({ message: "blockchain.js delete was hit!" }); 
     } catch (e) {
