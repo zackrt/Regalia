7bcd15c124ab9abc12962eb04344037c60ba6e25
ted <ted@warpedpuppy.com>
2018-02-07 14:07:45 -0800

    added unit tests

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { User } = require('../models/users');
+
+router.get('/', function(req, res) {
+  User
+	.find({})
+	.then(users => {
+		let userArray = users.map(function(user){return user.serialize()})
+		let obj = {users: userArray}
+		res.status(201).send(userArray)
+	})
+	.catch(err => {
+	  console.error("err");
+	  res.status(500).json({message: 'Internal server error'});
+	});
 });
 
+router.post('/', (req, res) => {
+  console.log('users post', req.body)
+  User
+    .create({
+      name: req.body.name
+    })
+    .then(
+      user => res.status(201).json(user.serialize()))
+    .catch(err => {
+      console.error("err");
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
 module.exports = router;
