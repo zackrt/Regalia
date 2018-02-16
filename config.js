c3f59ea2aa4530c0ac89a0b5836e161b1c94677d
ted <ted@warpedpuppy.com>
2018-02-16 13:56:41 -0800

    removed config

--- config.js
+++
@@ -1,7 +0,0 @@
-
-'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://userTest:userTest@ds255787.mlab.com:55787/warpedpuppy-node-restaurant-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/users';
-exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
