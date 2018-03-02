5b44ecc40b351c837bffcf4b1f372e06cc652fe5
Zack Toth <zacharyrtoth@gmail.com>
2018-03-01 22:19:52 -0800

    added chai tests

--- tests/test-regalia.js
+++ tests/test-regalia.js
@@ -9,8 +9,8 @@ const mongoose = require('mongoose');
 // this module
 const should = chai.should();
 
-const { Users } = require('../models');
-const { closeServer, runServer, app } = require('../server');
+const { Users } = require('../models/users');
+const { closeServer, runServer, app } = require('../app');
 const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
@@ -32,34 +32,35 @@ function tearDownDb() {
 // used to put randomish documents in db
 // so we have data to work with and assert about.
 // we use the Faker library to automatically
-// generate placeholder values for Email Address, title, content
+// generate placeholder values for Email Address, Password, first name, last name
 // and then we insert that data into mongo
-function seedBlogPostData() {
-  console.info('seeding blog post data');
+function seedUserData() {
+  console.info('seeding regalia users data');
   const seedData = [];
   for (let i = 1; i <= 10; i++) {
     seedData.push({
-      author: {
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName()
+      Users: {
+        EmailAddress: faker.EmailAddress(),
+        FirstName: faker.name.FirstName(),
+        LastName: faker.name.LastName()
       },
-      title: faker.lorem.sentence(),
-      content: faker.lorem.text()
+      RentPayment: faker.number(),
+      Password: faker.text()
     });
   }
   // this will return a promise
-  return BlogPost.insertMany(seedData);
+  return User.insertMany(seedData);
 }
 
 
-describe('blog posts API resource', function () {
+describe('regalia posts API resource', function () {
 
   before(function () {
     return runServer(TEST_DATABASE_URL);
   });
 
   beforeEach(function () {
-    return seedBlogPostData();
+    return seedUserData();
   });
 
   afterEach(function () {
@@ -77,7 +78,7 @@ describe('blog posts API resource', function () {
   // on proving something small
   describe('GET endpoint', function () {
 
-    it('should return all existing posts', function () {
+    it('should return all existing user', function () {
       // strategy:
       //    1. get back all posts returned by by GET request to `/posts`
       //    2. prove res has right status, data type
@@ -85,14 +86,14 @@ describe('blog posts API resource', function () {
       //       in db.
       let res;
       return chai.request(app)
-        .get('/posts')
+        .get('/users')
         .then(_res => {
           res = _res;
           res.should.have.status(200);
           // otherwise our db seeding didn't work
           res.body.should.have.length.of.at.least(1);
 
-          return BlogPost.count();
+          return Users.count();
         })
         .then(count => {
           // the number of returned posts should be same
@@ -106,7 +107,7 @@ describe('blog posts API resource', function () {
 
       let resPost;
       return chai.request(app)
-        .get('/posts')
+        .get('/users')
         .then(function (res) {
 
           res.should.have.status(200);
@@ -116,17 +117,18 @@ describe('blog posts API resource', function () {
 
           res.body.forEach(function (post) {
             post.should.be.a('object');
-            post.should.include.keys('id', 'title', 'content', 'author', 'created');
+            post.should.include.keys('id', 'EmailAddress', 'FirstName', 'LastName', 'password', 'RentPayment');
           });
           // just check one of the posts that its values match with those in db
           // and we'll assume it's true for rest
           resPost = res.body[0];
-          return BlogPost.findById(resPost.id);
+          return Users.findById(resPost.id);
         })
         .then(post => {
-          resPost.title.should.equal(post.title);
-          resPost.content.should.equal(post.content);
-          resPost.author.should.equal(post.authorName);
+          resPost.EmailAddress.should.equal(post.EmailAddress);
+          resPost.FirstName.should.equal(post.FirstName);
+          resPost.LastName.should.equal(post.LastName);
+          resPost.RentPayment.should.equal(post.RentPayment);
         });
     });
   });
@@ -136,39 +138,39 @@ describe('blog posts API resource', function () {
     // then prove that the post we get back has
     // right keys, and that `id` is there (which means
     // the data was inserted into db)
-    it('should add a new blog post', function () {
+    it('should create a new user account', function () {
 
-      const newPost = {
-        title: faker.lorem.sentence(),
-        author: {
-          firstName: faker.name.firstName(),
-          lastName: faker.name.lastName(),
-        },
-        content: faker.lorem.text()
+      const newUser = {
+        EmailAddress: faker.text.EmailAddress(),
+        password: faker.text.password(),
+        FirstName: faker.name.FirstName(),
+        LastName: faker.name.LastName(),
+        RentPayment: faker.number.RentPayment()
       };
 
       return chai.request(app)
-        .post('/posts')
-        .send(newPost)
+        .post('javascripts/homescreen.js')
+        .send(newUser)
         .then(function (res) {
           res.should.have.status(201);
           res.should.be.json;
           res.body.should.be.a('object');
           res.body.should.include.keys(
-            'id', 'title', 'content', 'author', 'created');
-          res.body.title.should.equal(newPost.title);
+            'id', 'EmailAddress', 'FirstName', 'LastName', 'password', 'RentPayment');
+          res.body.EmailAddress.should.equal(newUser.EmailAddress);
           // cause Mongo should have created id on insertion
           res.body.id.should.not.be.null;
-          res.body.author.should.equal(
-            `${newPost.author.firstName} ${newPost.author.lastName}`);
-          res.body.content.should.equal(newPost.content);
-          return BlogPost.findById(res.body.id);
+          res.body.FirstName.should.equal(
+            `${newUser.FirstName} ${newPost.LastName}`);
+          res.body.RentPayment.should.equal(newUser.RentPayment);
+          return User.findById(res.body.id);
         })
         .then(function (post) {
-          post.title.should.equal(newPost.title);
-          post.content.should.equal(newPost.content);
-          post.author.firstName.should.equal(newPost.author.firstName);
-          post.author.lastName.should.equal(newPost.author.lastName);
+          // to test with New User & CHANGE POST PARAMETER??
+          post.EmailAddress.should.equal(newUser.EmailAddress);
+          post.RentPayment.should.equal(newUser.RentPayment);
+          post.FirstName.should.equal(newUser.FirstName);
+          post.LastName.should.equal(newUser.LastName);
         });
     });
   });
@@ -181,32 +183,31 @@ describe('blog posts API resource', function () {
     //  4. Prove post in db is correctly updated
     it('should update fields you send over', function () {
       const updateData = {
-        title: 'cats cats cats',
-        content: 'dogs dogs dogs',
-        author: {
-          firstName: 'foo',
-          lastName: 'bar'
-        }
+        EmailAddress: 'TomSawyer@regalia.com',
+        FirstName: 'Tom',
+        LastName: 'Sawyer',
+        RentPayment: '1400'
       };
 
-      return BlogPost
+      return User
         .findOne()
-        .then(post => {
-          updateData.id = post.id;
+        .then(user => {
+          updateData.id = user.id;
 
           return chai.request(app)
-            .put(`/posts/${post.id}`)
+            .put(`/homescreen/${user.id}`)
             .send(updateData);
         })
         .then(res => {
           res.should.have.status(204);
-          return BlogPost.findById(updateData.id);
+          return User.findById(updateData.id);
         })
+        //CHANGE POST PARAMETER??
         .then(post => {
-          post.title.should.equal(updateData.title);
-          post.content.should.equal(updateData.content);
-          post.author.firstName.should.equal(updateData.author.firstName);
-          post.author.lastName.should.equal(updateData.author.lastName);
+          post.EmailAddress.should.equal(updateData.EmailAddress);
+          post.RentPayment.should.equal(updateData.RentPayment;
+          post.FirstName.should.equal(updateData.FirstName);
+          post.LastName.should.equal(updateData.LastName);
         });
     });
   });
