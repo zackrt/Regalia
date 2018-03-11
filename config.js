a3992656cb6d44263c72e6e7456fe894535b6689
Zack Toth <zacharyrtoth@gmail.com>
2018-03-11 10:14:40 -0700

    removed config.js from gitignore

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://RegaliaClean:development2018@ds015325.mlab.com:15325/zackrt-mongodb-mlab';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/users';
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
